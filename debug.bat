692bb3d24bcf3b1b0eaea463a6d5de9336f1b506
tldyl <756560020@qq.com>
2020-02-26 16:23:26 +0800

    untrack some files.

--- debug.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d E:\Steam\steamapps\common\SlayTheSpire
-start java -Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=y -jar ModTheSpire.jar
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d E:\Steam\steamapps\common\SlayTheSpire
-java -Xmx256m -jar ModTheSpire.jar
